c882c49791c95c7ba253e8a8c683db8e8d7703c9
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2025-07-09 01:24:23 +0500

    init

---
+++ index.ts
@@ -0,0 +1,33 @@
+import {Client, GatewayIntentBits, Partials} from "discord.js";
+import axios from "axios";
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+    partials: [Partials.Message], // allow fetching partial messages
+})
+
+client.once('ready', () => console.log(`Logged in as ${client.user?.tag}`));
+
+client.on('messageCreate', async message => {
+    if (message.author.bot) return;
+
+        try {
+            await axios.post("https://forixo.app.n8n.cloud/webhook/discord",{
+                content: message.content,
+                attachments: message.attachments,
+                messageId: message.id,
+                channelId: message.channel.id,
+                guildId: message.guild?.id,
+                userId: message.author.id,
+                username: message.author.username,
+                name: message.author.displayName,
+            })
+        } catch (err) {
+            console.error('Fetch failed:', err);
+            return;
+        }
+
+    console.log('Got new message:', message.content);
+});
+
+client.login(process.env.TOKEN);
